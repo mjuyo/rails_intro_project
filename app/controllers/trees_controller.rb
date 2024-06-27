dc57e504825bce4a179387e549d94e2439daeaef
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-06-27 10:35:05 -0500

    Search functionality added

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -6,4 +6,12 @@ class TreesController < ApplicationController
   def show
     @tree = Tree.find(params[:id])
   end
+
+  def search
+    if params[:park_id].present?
+      @trees = Tree.where(park: params[:park_id]).where("common LIKE ?", "%#{params[:query]}%")
+    else
+      @trees = Tree.where("common LIKE ?", "%#{params[:query]}%")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'about/index'
   resources :parks, only: [:index, :show]
   resources :trees, only: [:index, :show]
+  get 'search_trees', to: 'trees#search'
   get 'about', to: 'about#index'
   root 'parks#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
